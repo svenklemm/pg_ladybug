564cf15d3cd76ccea6f25cf39edc8d2ab0de3304
Sven Klemm <sven@timescale.com>
2025-04-06 16:27:57 +0200

    Some refactoring

--- src/postgres/PostgresCheck.cpp
+++ src/postgres/PostgresCheck.cpp
@@ -1,3 +1,4 @@
+#include "clang/Basic/Diagnostic.h"
 #include "clang-tidy/ClangTidy.h"
 #include "clang-tidy/ClangTidyCheck.h"
 #include "clang-tidy/ClangTidyModule.h"
@@ -16,58 +17,61 @@ class BitmapsetCheck: public ClangTidyCheck {
 public:
   BitmapsetCheck(StringRef Name, ClangTidyContext *Context)
       : ClangTidyCheck(Name, Context) {}
-  void registerMatchers(ast_matchers::MatchFinder *Finder) override;
-  void check(const ast_matchers::MatchFinder::MatchResult &Result) override;
+
+  void registerMatchers(MatchFinder *Finder) override {
+    Finder->addMatcher(callExpr().bind("bms_functions"), this);
+  }
+
+  void check(const MatchFinder::MatchResult &Result) override {
+    if (const CallExpr *callExpr = Result.Nodes.getNodeAs<CallExpr>("bms_functions"))
+    {
+      SourceLocation loc = callExpr->getBeginLoc();
+
+      const FunctionDecl *funcDecl = callExpr->getDirectCallee();
+      if (!funcDecl)
+        return;
+      std::string functionName = funcDecl->getNameInfo().getAsString();
+
+      if (functionName == "bms_make_singleton") {
+        this->verify_bitmapset_member(loc, functionName, callExpr->getArg(0));
+      }
+      if (functionName == "bms_add_member" || functionName == "bms_del_member") {
+        this->verify_bitmapset_member(loc, functionName, callExpr->getArg(1));
+      }
+      if (functionName == "bms_add_range") {
+        this->verify_bitmapset_member(loc, functionName, callExpr->getArg(1));
+        this->verify_bitmapset_member(loc, functionName, callExpr->getArg(2));
+      }
+
+    }
+  }
+
 private:
-	void verify_bitmapset_member(const SourceRange callRange, const std::string functionName, const Expr *arg);
+  /*
+   * Adding anything but small ints to a bitmapset is usally a mistake and an Index was
+   * meant to be added instead. e.g. RelOptInfo->relid (Index) was meant to be added but the code
+   * is adding RangeTblEntry->relid (Oid).
+   */
+  void verify_bitmapset_member(SourceLocation loc, const std::string functionName, const Expr *arg)
+  {
+    QualType argType = arg->getType();
+
+    if (const ImplicitCastExpr *implicitCast = dyn_cast<ImplicitCastExpr>(arg)) {
+      QualType sourceType = implicitCast->getSubExpr()->getType();
+      if (sourceType != argType)
+      {
+        std::string typeName = sourceType.getAsString();
+
+        if (typeName != "AttrNumber" && typeName != "int16" && typeName != "uint16" && typeName != "Index")
+        {
+          diag(loc, "potential wrong function argument. %0 called with datatype %1", DiagnosticIDs::Error) << functionName << typeName;
+        }
+
+      }
+    }
+  }
+
 };
-
-void BitmapsetCheck::registerMatchers(MatchFinder *Finder) {
-  Finder->addMatcher(callExpr().bind("check_bms_functions"), this);
-}
-
-void BitmapsetCheck::check(const MatchFinder::MatchResult &Result) {
-  const auto *call = Result.Nodes.getNodeAs<CallExpr>("check_bms_functions");
-  const FunctionDecl *funcDecl = call->getDirectCallee();
-  if (!funcDecl)
-    return;
-  std::string functionName = funcDecl->getNameInfo().getAsString();
-
-  if (functionName == "bms_make_singleton") {
-		this->verify_bitmapset_member(call->getSourceRange(), functionName, call->getArg(0));
-  }
-  if (functionName == "bms_add_member" || functionName == "bms_del_member") {
-		this->verify_bitmapset_member(call->getSourceRange(), functionName, call->getArg(1));
-  }
-  if (functionName == "bms_add_range") {
-		this->verify_bitmapset_member(call->getSourceRange(), functionName, call->getArg(1));
-		this->verify_bitmapset_member(call->getSourceRange(), functionName, call->getArg(2));
-  }
-}
-
-/*
- * Adding anything but small ints to a bitmapset is usally a mistake and an Index was
- * meant to be added instead. e.g. RelOptInfo->relid (Index) was meant to be added but the code
- * is adding RangeTblEntry->relid (Oid).
- */
-void BitmapsetCheck::verify_bitmapset_member(const SourceRange callRange, const std::string functionName, const Expr *arg)
-{
-  QualType argType = arg->getType();
-
-  if (const ImplicitCastExpr *implicitCast = dyn_cast<ImplicitCastExpr>(arg)) {
-		QualType sourceType = implicitCast->getSubExpr()->getType();
-    if (sourceType != argType)
-		{
-			std::string typeName = sourceType.getAsString();
-
-			if (typeName != "AttrNumber" && typeName != "int16" && typeName != "uint16" && typeName != "Index")
-			{
-				diag(callRange.getBegin(), "function %0 called with %1 argument ") << functionName << sourceType.getAsString();
-			}
-
-		}
-  }
-}
 
 } // namespace PostgresCheck
 
@@ -86,7 +90,7 @@ namespace clang::tidy {
 
 // Register the module using this statically initialized variable.
 static ClangTidyModuleRegistry::Add<::PostgresCheckModule>
-    postgresCheckInit("postgres-check-module", "Adds 'postgres-check' checks.");
+    X("postgres-check-module", "Adds 'postgres-check' checks.");
 
 // This anchor is used to force the linker to link in the generated object file
 // and thus register the module.
